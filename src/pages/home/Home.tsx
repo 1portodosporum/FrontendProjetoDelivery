1320d4502759934c187455d5f4c5234ef3f9115f
brartioli <brunaartioli.ba@gmail.com>
2025-01-31 12:04:57 -0300

    :sparkles:feat: add Home.tsx and Home.css

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,19 @@
-function Home() {
-  return <div>Um Porto!</div>;
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full overflow-y-auto">
+      <section className="relative h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('src/assets/photo-1504674900247-0877df9cc836.webp')" }}> 
+        <div className="absolute inset-0 bg-black/60" />
+
+        <div className="relative text-center text-white px-4">
+          <h1 className="text-7xl md:text-7xl font-bold mb-6">Delivey Comida</h1>
+          <p className="text-xl md:text-2xl mb-8">comida blablablablablablabla</p>
+
+        </div>
+      </section>
+      
+    </div>
+  );
 }
 
-export default Home;
