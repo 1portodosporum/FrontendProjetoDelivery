ff0e77a8e7fa5f52f9623be9f8f96024243779ed
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-31 11:35:54 -0300

    :sparkles: feat: implement ProdutoServices for managing products with CRUD operations

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,74 @@
+import { createContext, ReactNode, useEffect, useState } from "react"
+import UsuarioLogin from "../models/UsuarioLogin"
+import UsuarioServices  from "../services/UsuarioServices"
+
+
+interface AuthContextProps {
+    usuario: UsuarioLogin
+    handleLogout(): void
+    handleLogin(usuario: UsuarioLogin): Promise<void>
+    isLoading: boolean
+}
+
+interface AuthProviderProps {
+    children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextProps)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+
+    const [usuario, setUsuario] = useState<UsuarioLogin>(() => {
+        const usuarioSalvo = localStorage.getItem("usuario");
+        
+        return usuarioSalvo
+          ? JSON.parse(usuarioSalvo)
+          : {
+            id: 0,
+            nome: "",
+            usuario: "",
+            senha: "",
+            foto: "",
+            token: ""
+            };
+        });
+
+    const usuarioServices = new UsuarioServices();
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        if(usuario.token){
+            localStorage.setItem("usuario", JSON.stringify(usuario));
+        }
+    })
+
+    async function handleLogin(usuarioLogin: UsuarioLogin) {
+        setIsLoading(true)
+        try {
+            await usuarioServices.loginUsuario(`/usuarios/logar`, usuarioLogin, setUsuario)
+            alert("Usuário foi autenticado com sucesso!")
+        } catch (error) {
+            alert("Os dados do Usuário estão inconsistentes!")
+        }
+        setIsLoading(false)
+    }
+
+    function handleLogout() {
+        setUsuario({
+            id: 0,
+            nome: "",
+            usuario: "",
+            senha: "",
+            foto: "",
+            token: ""
+        })
+        localStorage.removeItem("usuario")
+    }
+
+    return (
+        <AuthContext.Provider value={{ usuario, handleLogin, handleLogout, isLoading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
