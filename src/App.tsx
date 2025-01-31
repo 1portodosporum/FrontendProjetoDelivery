0df215461d722537ab9109f77cc41eecf841bc4b
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-31 15:39:37 -0300

    :art:style: format ToastContainer component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Cadastro from './pages/cadastro/Cadastro';
-import Login from './pages/login/Login';
 
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
-      <ToastContainer />
+    <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Cadastro />} />
+
         </Routes>
       </BrowserRouter>
     </>
