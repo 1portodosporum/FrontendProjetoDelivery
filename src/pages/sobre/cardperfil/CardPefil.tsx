fe97a5abf342a5b94753965053e57f9ea8b26b62
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-05 20:01:04 -0300

    :sparkles: feat: enhance CardPerfil and ListarPefil components with improved styling and layout adjustments

--- src/pages/sobre/cardperfil/CardPefil.tsx
+++ src/pages/sobre/cardperfil/CardPefil.tsx
@@ -10,21 +10,21 @@ const CardPerfil = ({desenvolvedor}: CardPerfilProps) => {
                 <div className="relative bg-white shadow-lg rounded-lg border w-80 pt-16 pb-5 px-6 text-center">
                     <div className="absolute top-[-60px] left-1/2 transform -translate-x-1/2">
                         <img
-                            className="w-[130px] h-[130px] rounded-full shadow-sm border-4 border-white"
+                            className="w-[130px] h-[130px] rounded-full shadow-sm border-4 border-gray-100 transition-transform duration-300 hover:scale-95"
                             src={desenvolvedor.imagem}
                             alt="Profile"
                         />
                     </div>
                     <h5 className="text-primary font-bold text-lg mt-3">{desenvolvedor.nome}</h5>
                     <p className="text-gray-600 text-sm mb-3">{desenvolvedor.funcao}</p>
-                    <div className="mt-4 flex justify-center space-x-4 bg-slate-200 w-full p-2 rounded-lg">
-                        <a href={desenvolvedor.linkedin} className="text-gray-800 hover:text-blue-600 text-2xl">
+                    <div className="mt-4 flex justify-center space-x-4 bg-gray-100 w-full p-2 rounded-lg shadow-sm">
+                        <a href={desenvolvedor.linkedin} className="text-gray-800 hover:text-blue-600 text-2xl transition-transform duration-300 hover:-translate-y-1">
                         <i className='bx bxl-linkedin'></i>
                         </a>
-                        <a href={desenvolvedor.github} className="text-gray-800 hover:text-purple-600 text-2xl">
-                            <i className='bx bxl-github'  ></i>
+                        <a href={desenvolvedor.github} className="text-gray-800 hover:text-purple-600 text-2xl transition-transform duration-300 hover:-translate-y-1">
+                            <i className='bx bxl-github'></i>
                         </a>
-                        <a href={desenvolvedor.instagram} className="text-gray-800 hover:text-pink-700 text-2xl">
+                        <a href={desenvolvedor.instagram} className="text-gray-800 hover:text-pink-700 text-2xl transition-transform duration-300 hover:-translate-y-1">
                             <i className="bx bxl-instagram"></i>
                         </a>
                     </div>
--- src/pages/sobre/listarperfil/ListarPefil.tsx
+++ src/pages/sobre/listarperfil/ListarPefil.tsx
@@ -3,12 +3,12 @@ import desenvolvedores from "../time/Desenvolvedores";
 
 const ListarPefil = () => {
     return (
-        <section className="h-screen bg-gray-100 flex flex-col items-center justify-center flex-wrap gap-6 px-20 py-11 ">
-            <h1 className="py-10 text-5xl font-bold text-center text-orange-500 mb-16 tracking-wide">Nosso time</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-7xl">
+        <section className="h-screen bg-gray-100 flex flex-col items-center justify-center flex-wrap gap-6 px-10 py-2 ">
+            <h1 className="py-1 text-5xl font-bold text-center text-orange-500 mb-16 tracking-wide">Nosso time</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full max-w-7xl">
                 {
-                    desenvolvedores.map((desenvolvedores) => (
-                        <CardPerfil key={desenvolvedores.nome} desenvolvedor={desenvolvedores} />
+                    desenvolvedores.map((desenvolvedor) => (
+                        <CardPerfil key={desenvolvedor.nome} desenvolvedor={desenvolvedor} />
                     ))
                 }
             </div>
