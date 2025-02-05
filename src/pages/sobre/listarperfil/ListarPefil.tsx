14453c52b433a1ce02e31ab9f48c5a949132b4bf
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-05 19:42:45 -0300

    :sparkles: feat: refactor Sobre component to include ListarPefil; update layout and structure

--- src/pages/sobre/listarperfil/ListarPefil.tsx
+++ src/pages/sobre/listarperfil/ListarPefil.tsx
@@ -3,7 +3,8 @@ import desenvolvedores from "../time/Desenvolvedores";
 
 const ListarPefil = () => {
     return (
-        <section className="h-screen bg-gray-100 flex items-center justify-center flex-wrap gap-6 px-20 py-11 ">
+        <section className="h-screen bg-gray-100 flex flex-col items-center justify-center flex-wrap gap-6 px-20 py-11 ">
+            <h1 className="py-10 text-5xl font-bold text-center text-orange-500 mb-16 tracking-wide">Nosso time</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-7xl">
                 {
                     desenvolvedores.map((desenvolvedores) => (
