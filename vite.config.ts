2edf7d7d84e7992b197cf47e6112cb4b8b6107d2
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-02-26 20:54:41 -0300

    :sparkles: feat: add PWA support with vite-plugin-pwa; include app manifest and icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Aki Fome',
+        short_name: 'Aki Fome App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/aikifomelaranjao.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/aikifomelaranjao.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 })
